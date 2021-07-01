f908bac81f6615966ac7027948cd07adb4000091
MCSuda <suder.michal97@gmail.com>
2021-07-01 13:59:11 +0200

    Update with loop while

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import datetime
 
 name = input("Tell me Your name: ")
 age = int(input("Tell me Your age: "))
+num = int(input("How many messages: "))
 
 date = datetime.datetime.now()
 year = (date.year - age) + 100
 
-print("Hello " + name + " You will be 100 in " + str(year))
+while num > 0:
+    print("Hello " + name + " You will be 100 years old in " + str(year))
+    num = num - 1
